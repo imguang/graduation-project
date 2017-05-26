644bea5ef36c3ceb539fb4a0e694d399ba13e9b2
imguang <342601183@qq.com>
2017-05-26 15:11:32 +0800

    检索策略修改

--- src/main/java/com/imguang/demo/web/service/LuceneQueryService.java
+++ src/main/java/com/imguang/demo/web/service/LuceneQueryService.java
@@ -153,5 +153,77 @@ public class LuceneQueryService {
 		return baseSearchResultVO;
 
 	}
+	/**
+	 * 查询策略
+	 * @param term
+	 *            查询词
+	 * @throws ParseException
+	 * @throws IOException
+	 */
+	public BaseSearchResultVO queryByTerm2(String term) throws IOException, ParseException {
+		
+		BaseSearchResultVO baseSearchResultVO = null;
+		
+		/*
+		 * 搜索策略 1.首先搜索名称如果搜到则返回 2.如果没有对应名称，则搜索对应属性
+		 * 3.如果没有搜索到，则使用word2vec发现新词
+		 * 实体排序使用PageRank算法
+		 */
+		// first
+		log.info("开始检索");
+		List<HitsEntity> hitsEntities = queryUtil.searchByPropertiesNameAndValue("name", term);
+		if (hitsEntities.size() > 0) {
+			log.info("命中名称检索，检索到" + hitsEntities.size() + "条记录！");
+			log.info(hitsEntities.toString());
+			//得到最高得分，进行封装
+			baseSearchResultVO = getFlagEntity(hitsEntities.get(0));
+			baseSearchResultVO.setSearchRelations(getSearchRelations(hitsEntities));
+			return baseSearchResultVO;
+		}
+		log.info("没有查询到实体，开始查询属性");
+		// 2. 对相应属性进行查询
+		// abstract_content etiology treatment_detail prevent nursing
+		// function conponent cause pervent
+		List<HitsEntity> hitsPropertiesEntity = new ArrayList<>();
+		for (String property : properties) {
+			List<HitsEntity> temHits = queryUtil.searchByPropertiesNameAndValue(property, term);
+			log.info("查询" + property + "属性：" + temHits);
+			for (HitsEntity hitsEntity : temHits) {
+				if (hitsPropertiesEntity.contains(hitsEntity)) {
+					for (HitsEntity hitsEntity2 : hitsPropertiesEntity) {
+						if (hitsEntity2.equals(hitsEntity)) {
+							hitsEntity2.setScore(hitsEntity.getScore() + hitsEntity2.getScore());
+							break;
+						}
+					}
+				} else {
+					hitsPropertiesEntity.add(hitsEntity);
+				}
+			}
+		}
+		//排序
+		Collections.sort(hitsPropertiesEntity);
+		log.info("查询完毕：" + hitsPropertiesEntity);
+		if(hitsPropertiesEntity.size() > 0){
+			baseSearchResultVO = getFlagEntity(hitsPropertiesEntity.get(0));
+			baseSearchResultVO.setSearchRelations(getSearchRelations(hitsPropertiesEntity));
+			return baseSearchResultVO;
+		}
+		//3 word2vec
+		Word2VecService word2VecService = new Word2VecService();
+		List<HitsEntity> word2vecHitsEntities = new ArrayList<>();
+		List<String> items = word2VecService.findWords(term);
+		for (String string : items) {
+			word2vecHitsEntities.addAll(queryUtil.searchByPropertiesNameAndValue("name",string));
+		}
+		//排序
+		Collections.sort(hitsPropertiesEntity);
+		if(word2vecHitsEntities.size() > 0){
+			baseSearchResultVO = getFlagEntity(word2vecHitsEntities.get(0));
+			baseSearchResultVO.setSearchRelations(getSearchRelations(word2vecHitsEntities));
+		}
+		return baseSearchResultVO;
+		
+	}
 
 }
